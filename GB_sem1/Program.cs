c170b796e15e55f1df08bf6bffdfa086db7f675b
Nadezhda <burikh.nadja@mail.ru>
2022-06-10 21:04:39 +0300

    первая задача

---
+++ GB_sem1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.WriteLine("Result is" + result);
